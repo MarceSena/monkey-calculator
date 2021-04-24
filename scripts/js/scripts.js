0224503b4222d5d05cbc281a37c61dcf9bcaa903
MarceSena <marcelinosena@outlook.com>
2021-04-24 13:55:42 -0400

    FEAT: add backend aplication

--- scripts/js/scripts.js
+++ scripts/js/scripts.js
@@ -1,3 +1,26 @@
+
 function calc(val){
-    document.getElementById()
+     document.form.resultInput.value +=val;
 }
+
+function getResult(){
+    let val = document.form.resultInput.value;
+    if (val){
+        document.form.resultValue.value = eval(document.form.resultInput.value);
+    }
+}
+
+function resetInput(){
+    document.form.resultInput.value = " ";
+    document.form.resultValue.value = " ";
+
+}
+ function handleBackspace(){
+     let val = document.form.resultInput.value;
+     if (val) {
+         val = val.substring(0, val.length - 1);
+         document.form.resultInput.value = val;
+         document.form.resultValue.value = " ";
+
+     } 
+ }
